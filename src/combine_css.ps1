1dfa48d8676e8f5b5a7a99ccbb1c8ccdb5335a3f
PH01L <phoil@osrsbox.com>
2017-05-27 15:03:58 +1200

    Added build scripts to combine and minify (compress) JS and CSS code for distribution - used powershell scripts and YUI Compressor. Made small modification to CSS code to style strings and included new class (osrs-tooltip) to allow styling. Updated sample pages - removed un-needed style sheet, and simplified examples. Added .gitignore for YUI Compressor and other future exclusions.

--- src/combine_css.ps1
+++ src/combine_css.ps1
@@ -1,1 +1,34 @@
-cat jquery-ui.css, additions.css | sc osrsbox-tooltips.css
+# This script requires:
+# 1) Java (at least JRE)
+# 2) YUI Compressor
+# YUI compressor as a JAR is available from: https://github.com/yui/yuicompressor/downloads
+# Download it, extract ZIP file, open yuicompressor-3.4.7/build
+# Copy "yuicompressor-2.4.7.jar" to same folder as this script
+
+# Combine all CSS files to temp.css
+cat jquery-ui.css, additions.css | sc temp.css
+
+# Minify the temp.css file using YUI Compressor
+java -jar .\yuicompressor-2.4.7.jar .\temp.css -o .\osrsbox-tooltips.css
+
+# Remove the temp.css file
+Remove-Item .\temp.css
+
+# Static header for new CSS file
+$header = "/*! OSRSBOX Tooltips
+*   Date: 27/05/2017
+*   Author: PH01L
+*   Website: osrsbox.com
+*   License: MIT
+*   Dependencies: JQuery UI CSS (included license below)
+*/
+"
+
+# Read in minified CSS file
+$file = Get-Content .\osrsbox-tooltips.css
+
+# Append the header to top of final file
+Set-Content .\osrsbox-tooltips.css –value $header, $file
+
+# Move final file to parent directory
+Move-Item -Path .\osrsbox-tooltips.css -Destination ..\.\osrsbox-tooltips.css -force
--- src/combine_js.ps1
+++ src/combine_js.ps1
@@ -1,1 +1,34 @@
-cat jquery-3.2.1.min.js, jquery-ui.js, additions.js | sc osrsbox-tooltips.js
+# This script requires:
+# 1) Java (at least JRE)
+# 2) YUI Compressor
+# YUI compressor as a JAR is available from: https://github.com/yui/yuicompressor/downloads
+# Download it, extract ZIP file, open yuicompressor-3.4.7/build
+# Copy "yuicompressor-2.4.7.jar" to same folder as this script
+
+# Combine all JS files to temp.css
+cat jquery-3.2.1.min.js, jquery-ui.js, additions.js | sc temp.js
+
+# Minify the temp.js file using YUI Compressor
+java -jar .\yuicompressor-2.4.7.jar .\temp.js -o .\osrsbox-tooltips.js
+
+# Remove the temp.js file
+Remove-Item .\temp.js
+
+# Static header for new JS file
+$header = "/*! OSRSBOX Tooltips
+*   Date: 27/05/2017
+*   Author: PH01L
+*   Website: osrsbox.com
+*   License: MIT
+*   Dependencies: JQuery, JQuery UI (included license below)
+*/
+"
+
+# Read in minified JS file
+$file = Get-Content .\osrsbox-tooltips.js
+
+# Append the header to top of final file
+Set-Content .\osrsbox-tooltips.js –value $header, $file
+
+# Move final file to parent directory
+Move-Item -Path .\osrsbox-tooltips.js -Destination ..\.\osrsbox-tooltips.js -force
