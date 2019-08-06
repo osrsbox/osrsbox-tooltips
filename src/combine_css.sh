0a34e5ba674500876b3a27bd1643e2b32b859e43
osrsbox <phoil@osrsbox.com>
2019-08-07 06:55:44 +1200

    Updated bash script workflow for updating distributable JS/CSS files. This closes #8.

--- src/combine_css.sh
+++ src/combine_css.sh
@@ -2,26 +2,51 @@
 # 1) Java (at least JRE)
 # 2) YUI Compressor
 # YUI compressor as a JAR is available from: https://github.com/yui/yuicompressor/downloads
-# Download it, extract ZIP file, open yuicompressor-3.4.7/build
-# Copy "yuicompressor-2.4.7.jar" to same folder as this script
+# Download it, extract ZIP file, open yuicompressor-3.4.8/build
+# Copy "yuicompressor-2.4.8.jar" to same folder as this script
+
+# Variable for the current project version and date last edited
+current_version="1.0.4"
+current_date="2019/08/07"
 
 # Combine all CSS files to temp.css
 cat jquery-ui.css additions.css > temp.css
 
 # Minify the temp.css file using YUI Compressor
-java -jar yuicompressor-2.4.7.jar temp.css -o temp2.css
+java -jar yuicompressor-2.4.8.jar temp.css -o osrsbox-tooltips.css
 
 # Remove the temp.css file
 rm temp.css
 
-# Append the header to top of final file
-echo -ne "/* OSRSBOX Tooltips */\n/* Date: 2017/12/23 */\n/* Author: PH01L */\n/* Website: osrsbox.com */\n/* License: MIT */\n/* Dependencies: JQuery UI CSS (included license below) */\n" > osrsbox-tooltips.css
+# Static header for new CSS file
+header="/* OSRSBOX Tooltips\n \
+*   Version: $current_version\n \
+*   Date: $current_date\n \
+*   Author: PH01L\n \
+*   Website: osrsbox.com\n \
+*   License: MIT\n \
+*   Dependencies: JQuery UI (included license below)\n \
+*/\n \
+"
 
-# Append minified CSS files
-cat temp2.css >> osrsbox-tooltips.css
+# Set the output file name with versioning
+output_filename="osrsbox-tooltips_$current_version.min.css"
 
-# Remove the temp2.css file
-rm temp2.css
+echo -e "$header" > $output_filename
+cat osrsbox-tooltips.css >> $output_filename
+
+rm osrsbox-tooltips.css
+
+# Get the file hash of osrsbox-tooltips.css
+hash=$(openssl dgst -sha384 -binary $output_filename | openssl base64 -A)
+
+# Document SHA384 hash value in Base64
+echo "$output_filename: sha384-$hash"
 
 # Move final file to parent directory
-mv osrsbox-tooltips.css ../docs/osrsbox-tooltips.css
+cp $output_filename ../docs/$output_filename
+
+# Also create a normal file without versioning (for legacy applications)
+cp $output_filename ../docs/osrsbox-tooltips.css
+
+rm $output_filename
--- src/combine_js.sh
+++ src/combine_js.sh
@@ -2,26 +2,51 @@
 # 1) Java (at least JRE)
 # 2) YUI Compressor
 # YUI compressor as a JAR is available from: https://github.com/yui/yuicompressor/downloads
-# Download it, extract ZIP file, open yuicompressor-3.4.7/build
-# Copy "yuicompressor-2.4.7.jar" to same folder as this script
+# Download it, extract ZIP file, open yuicompressor-3.4.8/build
+# Copy "yuicompressor-2.4.8.jar" to same folder as this script
 
-# Combine all JS files to temp.css
+# Variable for the current project version and date last edited
+current_version="1.0.4"
+current_date="2019/08/07"
+
+# Combine all JS files to temp.js
 cat jquery-3.2.1.min.js jquery-ui.js additions.js > temp.js
 
 # Minify the temp.js file using YUI Compressor
-java -jar yuicompressor-2.4.7.jar temp.js -o temp2.js
+java -jar yuicompressor-2.4.8.jar temp.js -o osrsbox-tooltips.js
 
 # Remove the temp.js file
 rm temp.js
 
-# Append the header to top of final file
-echo -ne "/* OSRSBOX Tooltips */\n/* Date: 2017/12/23 */\n/* Author: PH01L */\n/* Website: osrsbox.com */\n/* License: MIT */\n/* Dependencies: JQuery UI CSS (included license below) */" > osrsbox-tooltips.js
+# Static header for new JS file
+header="/* OSRSBOX Tooltips\n \
+*   Version: $current_version\n \
+*   Date: $current_date\n \
+*   Author: PH01L\n \
+*   Website: osrsbox.com\n \
+*   License: MIT\n \
+*   Dependencies: JQuery, JQuery UI (included license below)\n \
+*/\n \
+"
 
-# Append minified CSS files
-cat temp2.js >> osrsbox-tooltips.js
+# Set the output file name with versioning
+output_filename="osrsbox-tooltips_$current_version.min.js"
 
-# Remove the temp2.css file
-rm temp2.js
+echo -e "$header" > $output_filename
+cat osrsbox-tooltips.js >> $output_filename
+
+rm osrsbox-tooltips.js
+
+# Get the file hash of osrsbox-tooltips.js
+hash=$(openssl dgst -sha384 -binary $output_filename | openssl base64 -A)
+
+# Document SHA384 hash value in Base64
+echo "$output_filename: sha384-$hash"
 
 # Move final file to parent directory
-mv osrsbox-tooltips.js ../docs/osrsbox-tooltips.js
+cp $output_filename ../docs/$output_filename
+
+# Also create a normal file without versioning (for legacy applications)
+cp $output_filename ../docs/osrsbox-tooltips.js
+
+rm $output_filename
